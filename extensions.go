55de361a758e7821eb551d1e1c541d85b11f92f2
David Taylor <dmtaylor2011@gmail.com>
2023-11-11 15:05:53 -0800

    WIP adding more extensions

--- extensions.go
+++ extensions.go
@@ -1,6 +1,10 @@
 package cefevent
 
-import "strings"
+import (
+	"net"
+	"strconv"
+	"strings"
+)
 
 // Extensions represent the additional fields in a CEF event
 type Extensions struct {
@@ -15,6 +19,18 @@ type Extensions struct {
 
 	// DestinationServiceName The service targeted by this event. Example "sshd"
 	DestinationServiceName string
+
+	// DestinationTranslatedAddress Identifies the translated destination that the event refers to in an IP network
+	DestinationTranslatedAddress net.IP
+
+	// DestinationTranslatedPort Port after it was translated; for example, a firewall. Valid port numbers are 0 to 65535
+	DestinationTranslatedPort *uint
+
+	// DeviceDirection Any information about what direction the observed communication has taken. 0 for inbound, 1 for outbound
+	DeviceDirection *uint8
+
+	// DeviceDnsDomain The DNS domain part of the complete fully qualified domain name (FQDN)
+	DeviceDnsDomain string
 	// TODO add all extensions
 
 	// CustomExtensions includes non-standard mappings in the extension field. Keys in the map shouldn't overlap with fields in the
@@ -22,19 +38,32 @@ type Extensions struct {
 	CustomExtensions map[string]string
 }
 
+// String formats extension for including in CEF event
 func (e Extensions) String() string {
 	b := strings.Builder{}
 	if e.DeviceAction != "" {
-		b.WriteString("act=" + escapeExtensionField(e.DeviceAction))
+		b.WriteString("act=" + escapeExtensionField(e.DeviceAction) + " ")
 	}
 	if e.ApplicationProtocol != "" {
-		b.WriteString("app=" + escapeExtensionField(e.ApplicationProtocol))
+		b.WriteString("app=" + escapeExtensionField(e.ApplicationProtocol) + " ")
 	}
 	if e.DestinationDnsDomain != "" {
-		b.WriteString("destinationDnsDomain=" + escapeExtensionField(e.DestinationDnsDomain))
+		b.WriteString("destinationDnsDomain=" + escapeExtensionField(e.DestinationDnsDomain) + " ")
 	}
 	if e.DestinationServiceName != "" {
-		b.WriteString("destinationServiceName=" + escapeExtensionField(e.DestinationServiceName))
+		b.WriteString("destinationServiceName=" + escapeExtensionField(e.DestinationServiceName) + " ")
+	}
+	if str := e.DestinationTranslatedAddress.String(); str != "<nil>" {
+		b.WriteString("destinationTranslatedAddress=" + escapeExtensionField(str) + " ")
+	}
+	if e.DestinationTranslatedPort != nil {
+		b.WriteString("destinationTranslatedPort=" + strconv.FormatUint(uint64(*e.DestinationTranslatedPort), 10) + " ")
+	}
+	if e.DeviceDirection != nil {
+		b.WriteString("deviceDirection=" + strconv.FormatUint(uint64(*e.DeviceDirection), 10) + " ")
+	}
+	if e.DeviceDnsDomain != "" {
+		b.WriteString("deviceDnsDomain=" + e.DeviceDnsDomain + " ")
 	}
 	// TODO implement
 
